be5dcb90a1ed0417e4989cba839d6c771f1b23d2
Ashikur Rahman <ashikurrahman7194@gmail.com>
2024-08-19 17:34:25 +0600

    code shortly and dynamic

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
+/**
+ * @ Modern Tab JS
+ */
 
 
+function openTab(evt, tabName) {
 
-function openTab(evt, modernTab) {
-    // Hide all tab content and remove active class
-    document
-      .querySelectorAll(".tabcontent")
-      .forEach((tab) => (tab.style.display = "none"));
-    document
-      .querySelectorAll(".tablinks")
-      .forEach((link) => link.classList.remove("active"));
-
-    // Show the selected tab content and set the clicked tab as active
-    document.getElementById(modernTab).style.display = "block";
-    evt.currentTarget.classList.add("active");
-  }
-
-  // Open the default tab on page load
-  document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("defaultOpen").click();
+  document.querySelectorAll('.tabcontent').forEach((tab) => {
+    tab.style.display = 'none';
   });
+
+  document.querySelectorAll('.tablinks').forEach((link) => {
+    link.classList.remove('active');
+  });
+
+  document.getElementById(tabName).style.display = 'block';
+  evt.currentTarget.classList.add('active');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById("defaultOpen").click();
+})
